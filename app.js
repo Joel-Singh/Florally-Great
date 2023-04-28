25265d7b936e6ada9f5ba3d31c323376cb682eff
Joel Singh <joelsingh788@gmail.com>
2023-04-28 13:47:10 -0400

    Setup mongoose connection

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://Joel:Joel@flower-store.byzszdj.mongodb.net/?retryWrites=true&w=majority";
+
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
