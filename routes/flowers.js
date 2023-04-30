e9d2f3714fb7240c1e8f86b2f122bff07182cc6a
Joel Singh <joelsingh788@gmail.com>
2023-04-30 14:11:33 -0400

    Remove prepended path, as that'll be added on the use method

--- routes/flowers.js
+++ routes/flowers.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const flower_controller = require('../controllers/flower')
 
-router.get('/flowers', flower_controller.flower_list)
-router.get('/flowers/:name', flower_controller.flower_detail)
+router.get('/', flower_controller.flower_list)
+router.get('/:name', flower_controller.flower_detail)
 
-router.get('/flowers/create', flower_controller.flower_create_get)
-router.post('/flowers/create', flower_controller.flower_create_post)
+router.get('/create', flower_controller.flower_create_get)
+router.post('/create', flower_controller.flower_create_post)
 
-router.get('/flowers/:name/update', flower_controller.flower_update_get)
-router.post('/flowers/:name/update', flower_controller.flower_update_post)
+router.get('/:name/update', flower_controller.flower_update_get)
+router.post('/:name/update', flower_controller.flower_update_post)
 
 module.exports = router
