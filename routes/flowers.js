08df885360b054545b43a4ae9556b89a7d470aa6
Joel Singh <joelsingh788@gmail.com>
2023-05-02 12:51:09 -0400

    Move /:name route lower down so create routes work

--- routes/flowers.js
+++ routes/flowers.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const flower_controller = require('../controllers/flower')
 
 router.get('/', flower_controller.flower_list)
-router.get('/:name', flower_controller.flower_detail)
 
 router.get('/create', flower_controller.flower_create_get)
 router.post('/create', flower_controller.flower_create_post)
 
+router.get('/:name', flower_controller.flower_detail)
 router.get('/:name/update', flower_controller.flower_update_get)
 router.post('/:name/update', flower_controller.flower_update_post)
 
--- routes/regions.js
+++ routes/regions.js
@@ -5,14 +5,14 @@ const region_controller = require('../controllers/region')
 const flower_and_region_controller = require('../controllers/flower_and_region')
 
 router.get('/', region_controller.region_list)
+
+router.get('/create', region_controller.region_create_get)
+router.post('/create', region_controller.region_create_post)
+
+router.get('/create', region_controller.region_create_get)
+router.post('/create', region_controller.region_create_post)
+
 router.get('/:name', region_controller.region_detail)
-
-router.get('/create', region_controller.region_create_get)
-router.post('/create', region_controller.region_create_post)
-
-router.get('/create', region_controller.region_create_get)
-router.post('/create', region_controller.region_create_post)
-
 router.get('/:name/update', region_controller.region_update_get)
 router.post('/:name/update', region_controller.region_update_post)
 
