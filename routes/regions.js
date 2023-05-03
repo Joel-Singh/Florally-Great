f7dbb23879bd4ed57de32f0818e77b3594f9f87c
Joel Singh <joelsingh788@gmail.com>
2023-05-03 13:11:51 -0400

    Rename region_controller to regionController

--- routes/regions.js
+++ routes/regions.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const region_controller = require('../controllers/region')
+const regionController = require('../controllers/region')
 const flower_and_region_controller = require('../controllers/flower_and_region')
 
-router.get('/', region_controller.region_list)
+router.get('/', regionController.region_list)
 
-router.get('/create', region_controller.region_create_get)
-router.post('/create', region_controller.region_create_post)
+router.get('/create', regionController.region_create_get)
+router.post('/create', regionController.region_create_post)
 
-router.get('/create', region_controller.region_create_get)
-router.post('/create', region_controller.region_create_post)
+router.get('/create', regionController.region_create_get)
+router.post('/create', regionController.region_create_post)
 
-router.get('/:name', region_controller.region_detail)
-router.get('/:name/update', region_controller.region_update_get)
-router.post('/:name/update', region_controller.region_update_post)
+router.get('/:name', regionController.region_detail)
+router.get('/:name/update', regionController.region_update_get)
+router.post('/:name/update', regionController.region_update_post)
 
 module.exports = router
