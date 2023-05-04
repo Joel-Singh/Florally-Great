8d73b79c05598e095ee8f8321ad9488abe6e8f51
Joel Singh <joelsingh788@gmail.com>
2023-05-04 09:38:46 -0400

    Add all regions page

--- controllers/region.js
+++ controllers/region.js
@@ -2,7 +2,11 @@ const Region = require('../models/region')
 const asyncHandler = require('express-async-handler')
 
 exports.region_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Region list")
+  const allRegions = await Region.find({}, 'name description url')
+    .sort({ name: 1 })
+    .exec()
+
+  res.render('all_regions', { region_list: allRegions, title: 'All Regions'})
 })
 
 exports.region_detail = asyncHandler(async (req, res, next) => {
