6a75cda9f64df6d551cd190896cc6ff911c302cd
Joel Singh <joelsingh788@gmail.com>
2023-05-15 13:48:45 -0400

    Implement region_create_post

--- controllers/region.js
+++ controllers/region.js
@@ -1,5 +1,6 @@
 const Region = require('../models/region')
 const asyncHandler = require('express-async-handler')
+const { body, validationResult } = require("express-validator");
 
 exports.region_list = asyncHandler(async (req, res, next) => {
   const allRegions = await Region.find({}, 'name description url')
@@ -17,9 +18,39 @@ exports.region_create_get = asyncHandler(async (req, res, next) => {
   res.render('regions/region_form', {title: 'Add New Region'})
 })
 
-exports.region_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Region create POST")
-})
+exports.region_create_post = [
+  body('name', `Name can't be empty`)
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      res.render("regions/region_form", {
+        title: "Add New Region",
+        errors: errors.array()
+      })
+      return
+    } else {
+      const { name, description } = req.body
+      const regionExists = await Region.findOne({ name }).exec()
+
+      if (regionExists) {
+        res.render('regions/region_form', {
+          title: "Add New Region",
+          errors: [{ msg: 'Region already exists'}]
+        })
+      } else {
+        const region = new Region({name, description})
+        await region.save()
+        res.redirect(region.url)
+      }
+    }
+  })
+]
+
 
 exports.region_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Region update GET")
