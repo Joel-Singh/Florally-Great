c77b66b3696d5199e8d54528679e61505df34e5d
Joel Singh <joelsingh788@gmail.com>
2023-05-20 16:49:32 -0400

    Remove unnecessary comment

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const index_controller = require('../controllers/index')
 
-/* GET home page. */
 router.get('/', index_controller.index);
 
 module.exports = router;
