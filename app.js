2766b6020a31425526fdbb4781c3d980f3ba29d8
Joel Singh <joelsingh788@gmail.com>
2023-05-21 19:02:48 -0400

    Add appRoot global variable

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const path = require('path');
+global.appRoot = path.resolve(__dirname);
+
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
