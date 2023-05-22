335f14a46d05f45a89bc23a0402e06032cc5f8a1
Joel Singh <joelsingh788@gmail.com>
2023-05-22 11:34:54 -0400

    Move the checking of a duplicate flower into its own variable

--- controllers/flower/create_post.js
+++ controllers/flower/create_post.js
@@ -37,36 +37,41 @@ const validateInput = (req, res, next) => {
   next();
 }
 
+const checkDuplicateFlower = asyncHandler(async (req, res, next) => {
+  const { name } = req.body
+  const flowerExists = await Flower.findOne({ name }).exec()
+  if (flowerExists) {
+    res.render('flowers/flower_form', {
+      errors: [{ msg: 'Flower already exists'}]
+    })
+    return
+  }
+  next();
+})
+
 module.exports = [
   ...validationFunctions,
   validateInput,
+  checkDuplicateFlower,
 
   asyncHandler(async (req, res, next) => {
-      const { name, description, price, numberInStock, region } = req.body
-      const flowerExists = await Flower.findOne({ name }).exec()
+    const { name, description, price, numberInStock, region } = req.body
+      const priceToNumber = parseFloat(price.slice(1))
 
-      if (flowerExists) {
-        res.render('flowers/flower_form', {
-          errors: [{ msg: 'Flower already exists'}]
-        })
-      } else {
-        const priceToNumber = parseFloat(price.slice(1))
+      const convertedName = he.decode(name)
+      const convertedDescription = he.decode(description)
 
-        const convertedName = he.decode(name)
-        const convertedDescription = he.decode(description)
-
-        const flower = new Flower(
-          {
-            name: convertedName,
-            description: convertedDescription,
-            price: priceToNumber,
-            numberInStock,
-            region
-          }
-        )
-        await flower.save()
-        res.redirect(flower.url)
-      }
+      const flower = new Flower(
+        {
+          name: convertedName,
+          description: convertedDescription,
+          price: priceToNumber,
+          numberInStock,
+          region
+        }
+      )
+      await flower.save()
+      res.redirect(flower.url)
     }
   )
 ]
