4c3348872137d2fdee2a29815e90fd7448d80609
Joel Singh <joelsingh788@gmail.com>
2023-05-22 11:33:13 -0400

    Move the validation of input into a variable

--- controllers/flower/create_post.js
+++ controllers/flower/create_post.js
@@ -26,18 +26,22 @@ const validationFunctions = [
     .matches(/\$[0-9]+\.[0-9][0-9]/).withMessage('Price needs to be in $x.xx format, e.g $3.86 or $287.00'),
 ]
 
+const validateInput = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.render("flowers/flower_form", {
+      errors: errors.array()
+    })
+    return
+  }
+  next();
+}
+
 module.exports = [
   ...validationFunctions,
+  validateInput,
 
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req)
-
-    if (!errors.isEmpty()) {
-      res.render("flowers/flower_form", {
-        errors: errors.array()
-      })
-      return
-    } else {
       const { name, description, price, numberInStock, region } = req.body
       const flowerExists = await Flower.findOne({ name }).exec()
 
@@ -64,29 +68,5 @@ module.exports = [
         res.redirect(flower.url)
       }
     }
-  })
+  )
 ]
-
-function getValidationFunctions() {
-  return [
-    body('name')
-      .trim()
-      .isLength({ min: 1}).withMessage(`Name can't be empty`)
-      .escape(),
-
-    body('description')
-      .trim()
-      .isLength({ min: 1}).withMessage(`Description can't be empty`)
-      .escape(),
-
-    body('numberInStock')
-      .trim()
-      .isLength({ min: 1}).withMessage(`Number in stock can't be empty`)
-      .isNumeric().withMessage('Number in stock needs to be a number'),
-
-    body('price')
-      .trim()
-      .isLength({ min: 1}).withMessage(`Price can't be empty`)
-      .matches(/\$[0-9]+\.[0-9][0-9]/).withMessage('Price needs to be in $x.xx format, e.g $3.86 or $287.00'),
-  ]
-}
