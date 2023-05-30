15ade967eed969c84a9b36165f1b1e7f99efbe7d
Joel Singh <joelsingh788@gmail.com>
2023-05-30 06:54:56 -0400

    Add tests for delete_region view

---
+++ views/regions/__tests__/util/renderPugToDOM.js
@@ -0,0 +1,13 @@
+const pug = require('pug')
+const { JSDOM } = require('jsdom')
+
+module.exports = (filePath, locals) => {
+  const compiledTemplate = pug.compileFile(filePath);
+
+  const html = compiledTemplate(locals);
+
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  return document;
+}
