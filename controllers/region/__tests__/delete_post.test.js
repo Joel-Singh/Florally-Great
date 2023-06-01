7df37fca2737cef426874c9bc7c635662f5ea383
Joel Singh <joelsingh788@gmail.com>
2023-06-01 10:49:26 -0400

    Add test for region deletion

---
+++ controllers/region/__tests__/delete_post.test.js
@@ -0,0 +1,46 @@
+const path = require('path')
+
+const express = require("express");
+const mongoose = require('mongoose')
+const Region = require(path.join(appRoot, 'models', 'region.js'))
+const sendFormData = require(path.join(appRoot, 'testingUtils', 'sendFormData.js'))
+
+const delete_post = require('../delete_post')
+
+let app;
+const initializeInMemoryMongoDB = require(path.join(
+  appRoot, 'testingUtils', 'initializeInMemoryMongoDB.js')
+)
+
+beforeAll(async () => {
+  app = configureExpressApp()
+  await initializeInMemoryMongoDB()
+})
+
+test("Region is deleted", async () => {
+  const regionToBeDeleted = new Region({
+    name: 'Region Name',
+    description: "I'm gonna be deleted!"
+  })
+
+  const regionToBeDeletedDoc = await regionToBeDeleted.save()
+  const id = regionToBeDeletedDoc._id
+
+  await sendFormData(app, '/', { region: id})
+
+  const foundRegion = await Region.findById(id).exec()
+  expect(foundRegion).toBeNull()
+});
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
+
+function configureExpressApp() {
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use("/", delete_post);
+
+  return app
+}
---
+++ testingUtils/sendFormData.js
@@ -0,0 +1,23 @@
+const request = require('supertest')
+
+module.exports = async function(app, endpoint, data) {
+  await request(app)
+    .post(endpoint)
+    .send(createFormDataString(data))
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+}
+
+function createFormDataString(data) {
+  let formString = '';
+
+  Object.entries(data).forEach(([prop, value]) => {
+    formString += `${prop}=${value}&`
+  });
+
+  formString = removeTrailingAmpersand(formString)
+  return formString
+
+  function removeTrailingAmpersand(string) {
+    return string.slice(0, -1)
+  }
+}
