34b8b39d486ade0acaf26dae16e80a41fa6486bc
Joel Singh <joelsingh788@gmail.com>
2023-06-01 11:13:07 -0400

    Remove unnecessary function

--- controllers/flower/util/renderFlowerForm.js
+++ controllers/flower/util/renderFlowerForm.js
@@ -3,15 +3,10 @@ const asyncHandler = require('express-async-handler')
 const Region = require(path.join(appRoot, 'models', 'region.js'))
 
 module.exports = asyncHandler(async function(res, next, viewContext) {
-  const allRegionNames = await getAllRegionNames(next)
+  const allRegionNames = await Region.find({}, 'name').exec()
 
   res.render('flowers/flower_form', {
     ...viewContext,
     regionList: allRegionNames,
   })
 })
-
-const getAllRegionNames = async function() {
-  const allRegions = await Region.find({}, 'name').exec()
-  return allRegions
-}
