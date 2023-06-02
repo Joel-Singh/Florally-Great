4b2fdc71c47c4ca17d0efb75d0de12e70b0d14f9
Joel Singh <joelsingh788@gmail.com>
2023-06-02 09:29:24 -0400

    Modularize the adding of app middleware

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
 const path = require('path');
 global.appRoot = path.resolve(__dirname);
 
-const createError = require('http-errors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const mongoose = require("mongoose");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const flowersRouter = require('./routes/flowers')
-const regionsRouter = require('./routes/regions')
+const viewEngineSetup = require('./appMiddlewares/viewEngineSetup.js')
+const addGeneralMiddleware = require('./appMiddlewares/addGeneralMiddleware.js')
+const addRoutes = require('./appMiddlewares/addRoutes.js')
+const addErrorHandlingMiddleware = require('./appMiddlewares/addErrorHandlingMiddleware.js')
 
 connectToMongoDB()
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/flowers', flowersRouter)
-app.use('/regions', regionsRouter)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+viewEngineSetup(app)
+addGeneralMiddleware(app)
+addRoutes(app)
+addErrorHandlingMiddleware(app)
 
 module.exports = app;
 
---
+++ appMiddlewares/addRoutes.js
@@ -0,0 +1,11 @@
+const indexRouter = require('../routes/index');
+const usersRouter = require('../routes/users');
+const flowersRouter = require('../routes/flowers')
+const regionsRouter = require('../routes/regions')
+
+module.exports = function(app) {
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  app.use('/flowers', flowersRouter)
+  app.use('/regions', regionsRouter)
+}
