28daf4edd121a22be4bf93abc47202136c84049e
Joel Singh <joelsingh788@gmail.com>
2023-06-05 14:53:46 -0400

    Add test for form error rendering

---
+++ views/__tests__/form.test.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const renderPugToDOM = require(path.join(
+  appRoot,
+  "views",
+  "renderPugToDOM.js"
+));
+
+test.each([
+  ["Form renders", {}],
+  [
+    "Form renders a single error",
+    { errors: [{ msg: "This Error Msg Should be seen" }] },
+  ],
+  [
+    "Form renders multiple errors",
+    {
+      errors: [
+        { msg: "This Error Msg Should be seen 1" },
+        { msg: "This Error Msg Should be seen 2" },
+        { msg: "This Error Msg Should be seen 3" },
+      ],
+    },
+  ],
+])("%s", (name, locals) => {
+  const form = renderPugToDOM("./views/form.pug", locals).querySelector(
+    "form"
+  );
+  expect(form).toMatchSnapshot();
+});
