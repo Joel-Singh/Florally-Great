4c787c694daf89326fd8efac246dd2334892f59d
Joel Singh <joelsingh788@gmail.com>
2023-06-12 15:36:52 -0400

    Test the passing of flowers in all_flowers_in_region

--- controllers/region/__tests__/all_flowers_in_region.test.js
+++ controllers/region/__tests__/all_flowers_in_region.test.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const Region = require(path.join(appRoot, "models", "region.js"));
+const Flower = require(path.join(appRoot, "models", "flower.js"));
 
 const all_flowers_in_region = require("../all_flowers_in_region.js");
 
@@ -40,4 +41,38 @@ test("Passes in just region", async () => {
       ],
     ]
   `);
+});
+
+test("Passes in region and flowers", async () => {
+  const regionName = "regionName";
+  const regionId = "6483d106cdcd7f4f8d6ed46a";
+
+  const fakeReq = {
+    params: {
+      name: regionName,
+    },
+  };
+
+  const fakeRes = {
+    render: jest.fn(),
+  };
+
+  await new Region({
+    name: regionName,
+    description: "description",
+    _id: "6483d106cdcd7f4f8d6ed46a",
+  }).save();
+
+  await new Flower({
+    name: "Flower in region",
+    description: "description",
+    price: 32,
+    numberInStock: 92,
+    region: regionId,
+    _id: "6487734ae98dc419fc0f170d"
+  }).save();
+
+  await all_flowers_in_region(fakeReq, fakeRes);
+
+  expect(fakeRes.render.mock.calls).toMatchSnapshot();
 });
