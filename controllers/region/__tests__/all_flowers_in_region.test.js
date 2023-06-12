098b4172b700f62860435dc3bb99eafa332493ea
Joel Singh <joelsingh788@gmail.com>
2023-06-12 15:49:05 -0400

    Modularize tests for all_flowers_in_region

--- controllers/region/__tests__/all_flowers_in_region.test.js
+++ controllers/region/__tests__/all_flowers_in_region.test.js
@@ -7,46 +7,30 @@ const all_flowers_in_region = require("../all_flowers_in_region.js");
 
 test("Passes in just region", async () => {
   const regionName = "regionName";
-  const fakeReq = {
-    params: {
-      name: regionName,
-    },
-  };
 
-  const fakeRes = {
-    render: jest.fn(),
-  };
+  const { fakeReq, fakeRes } = getFakeMiddlewareParameters(regionName)
 
-  await new Region({
-    name: regionName,
-    description: "description",
-    _id: "6483d106cdcd7f4f8d6ed46a",
-  }).save();
+  await addRegionToDb(regionName)
 
   await all_flowers_in_region(fakeReq, fakeRes);
 
-  expect(fakeRes.render.mock.calls).toMatchInlineSnapshot(`
-    [
-      [
-        "regions/all_flowers_in_region",
-        {
-          "flower_list": [],
-          "region": {
-            "__v": 0,
-            "_id": "6483d106cdcd7f4f8d6ed46a",
-            "description": "description",
-            "name": "regionName",
-          },
-        },
-      ],
-    ]
-  `);
+  snapshotRenderCall(fakeRes)
 });
 
 test("Passes in region and flowers", async () => {
   const regionName = "regionName";
-  const regionId = "6483d106cdcd7f4f8d6ed46a";
 
+  const { fakeReq, fakeRes } = getFakeMiddlewareParameters(regionName);
+
+  const regionId = await addRegionToDb(regionName);
+  await addFlowerToDb(regionId);
+
+  await all_flowers_in_region(fakeReq, fakeRes);
+
+  snapshotRenderCall(fakeRes);
+});
+
+function getFakeMiddlewareParameters(regionName) {
   const fakeReq = {
     params: {
       name: regionName,
@@ -57,22 +41,31 @@ test("Passes in region and flowers", async () => {
     render: jest.fn(),
   };
 
+  return { fakeReq, fakeRes };
+}
+
+async function addRegionToDb(regionName) {
+  const id = "6483d106cdcd7f4f8d6ed46a"
   await new Region({
     name: regionName,
     description: "description",
     _id: "6483d106cdcd7f4f8d6ed46a",
   }).save();
 
+  return id
+}
+
+async function addFlowerToDb(flowersRegionAsId) {
   await new Flower({
     name: "Flower in region",
     description: "description",
     price: 32,
     numberInStock: 92,
-    region: regionId,
+    region: flowersRegionAsId,
     _id: "6487734ae98dc419fc0f170d"
   }).save();
+}
 
-  await all_flowers_in_region(fakeReq, fakeRes);
-
+function snapshotRenderCall(fakeRes) {
   expect(fakeRes.render.mock.calls).toMatchSnapshot();
-});
+}
