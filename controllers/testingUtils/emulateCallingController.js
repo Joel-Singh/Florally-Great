f0cf14987169f3be01d06762e6e6fa8bb1e7f731
Joel Singh <joelsingh788@gmail.com>
2023-06-20 14:48:04 -0400

    Add redirect property on fakeRes

--- controllers/testingUtils/emulateCallingController.js
+++ controllers/testingUtils/emulateCallingController.js
@@ -16,6 +16,7 @@ module.exports = async function(controller, reqProperties, resProperties) {
 
     const fakeRes = {
       render: jest.fn(),
+      redirect: jest.fn(),
       ...resProperties
     };
 
