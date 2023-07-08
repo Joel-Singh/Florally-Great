c4cd44c3ccf5ac99769cedea86c24e24c814c268
Joel Singh <joelsingh788@gmail.com>
2023-07-07 22:49:50 -0400

    Nest test validation utility function

--- controllers/flower/__tests__/create_post.test.js
+++ controllers/flower/__tests__/create_post.test.js
@@ -5,6 +5,23 @@ const { saveDummyRegion } = require("../../testingUtils/savingDummyDataToDb");
 const Region = require("../../../models/region");
 
 describe("Test validation", () => {
+  async function getFilteredValidationErrors(name, value, msgMustInclude) {
+    const { fakeReq } = await emulateCallingController(create_post, {
+      body: {
+        [name]: value,
+      },
+    });
+
+    const resultOfValidation = validationResult(fakeReq);
+    const errorsForJustProperty = resultOfValidation.errors.filter(
+      ({ path, msg }) => {
+        return path === name && msg.includes(msgMustInclude);
+      }
+    );
+
+    return errorsForJustProperty;
+  }
+
   const flowerProperties = ["name", "description", "numberInStock", "price"];
   test.each(flowerProperties)(
     `Doesn't accept empty input for %s`,
@@ -77,20 +94,3 @@ describe("Test validation", () => {
     ).toEqual([]);
   });
 });
-
-async function getFilteredValidationErrors(name, value, msgMustInclude) {
-  const { fakeReq } = await emulateCallingController(create_post, {
-    body: {
-      [name]: value,
-    },
-  });
-
-  const resultOfValidation = validationResult(fakeReq);
-  const errorsForJustProperty = resultOfValidation.errors.filter(
-    ({ path, msg }) => {
-      return path === name && msg.includes(msgMustInclude);
-    }
-  );
-
-  return errorsForJustProperty;
-}
