f49dd882d25e951e2cf184cfdb9e78ff4feff63e
Joel Singh <joelsingh788@gmail.com>
2023-07-11 14:26:25 -0400

    Remove unnecessary describe block

--- controllers/flower/util/__tests__/validateFlowerRequest.test.js
+++ controllers/flower/util/__tests__/validateFlowerRequest.test.js
@@ -7,99 +7,98 @@ const {
 } = require("../../../testingUtils/dummyData/savingDummyDataToDb.js");
 const getValidFlowerPostData = require("../../../testingUtils/dummyData/getValidFlowerPostData.js");
 
-describe("Test validation", () => {
-  async function getValidationErrors(bodyOverwrites = {}, errorMsgMustInclude) {
-    const { fakeReq } = await emulateCallingController(validateFlowerRequest, {
-      body: {
-        ...(await getValidFlowerPostData()),
-        ...bodyOverwrites,
-      },
+async function getValidationErrors(bodyOverwrites = {}, errorMsgMustInclude) {
+  const { fakeReq } = await emulateCallingController(validateFlowerRequest, {
+    body: {
+      ...(await getValidFlowerPostData()),
+      ...bodyOverwrites,
+    },
+  });
+
+  const errors = validationResult(fakeReq).errors;
+  if (typeof errorMsgMustInclude === "undefined") return errors;
+  else
+    return errors.filter(({ msg }) => {
+      return msg.includes(errorMsgMustInclude);
     });
+}
 
-    const errors = validationResult(fakeReq).errors;
-    if (typeof errorMsgMustInclude === "undefined") return errors;
-    else
-      return errors.filter(({ msg }) => {
-        return msg.includes(errorMsgMustInclude);
-      });
+test(`No errors with valid data`, async () => {
+  expect(await getValidationErrors()).toEqual([]);
+});
+
+const flowerProperties = ["name", "description", "numberInStock", "price"];
+test.each(flowerProperties)(
+  `Doesn't accept empty input for %s`,
+  async (property) => {
+    expect(
+      await getValidationErrors(
+        {
+          [property]: "",
+        },
+        "empty"
+      )
+    ).toMatchSnapshot();
   }
+);
 
-  test(`No errors with valid data`, async () => {
-    expect(await getValidationErrors()).toEqual([]);
-  });
-
-  const flowerProperties = ["name", "description", "numberInStock", "price"];
-  test.each(flowerProperties)(
-    `Doesn't accept empty input for %s`,
-    async (property) => {
-      expect(
-        await getValidationErrors(
-          {
-            [property]: "",
-          },
-          "empty"
-        )
-      ).toMatchSnapshot();
-    }
-  );
-
-  test.each(flowerProperties)(
-    `Doesn't accept just spaces for %s`,
-    async (property) => {
-      expect(
-        await getValidationErrors(
-          {
-            [property]: "   ",
-          },
-          "empty"
-        )
-      ).toMatchSnapshot();
-    }
-  );
-
-  test(`Only accepts numbers for number in stock`, async () => {
-    expect(
-      await getValidationErrors({
-        numberInStock: 32,
-      })
-    ).toEqual([]);
-
-    expect(
-      await getValidationErrors({
-        numberInStock: "not a number",
-      })
-    ).toMatchSnapshot();
-  });
-
-  test(`Only accepts property formatted prices`, async () => {
+test.each(flowerProperties)(
+  `Doesn't accept just spaces for %s`,
+  async (property) => {
     expect(
       await getValidationErrors(
         {
-          price: 32,
+          [property]: "   ",
         },
-        "format"
+        "empty"
       )
     ).toMatchSnapshot();
+  }
+);
 
-    expect(
-      await getValidationErrors(
-        {
-          price: "$3.86",
-        },
-        "format"
-      )
-    ).toEqual([]);
-  });
+test(`Only accepts numbers for number in stock`, async () => {
+  expect(
+    await getValidationErrors({
+      numberInStock: 32,
+    })
+  ).toEqual([]);
 
-  test(`Doesn't accept nonexistent regions`, async () => {
-    expect(
-      await getValidationErrors(
-        {
-          regionID: "asdfasdf",
-        },
-        "Region does not exist"
-      )
-    ).toMatchInlineSnapshot(`
+  expect(
+    await getValidationErrors({
+      numberInStock: "not a number",
+    })
+  ).toMatchSnapshot();
+});
+
+test(`Only accepts property formatted prices`, async () => {
+  expect(
+    await getValidationErrors(
+      {
+        price: 32,
+      },
+      "format"
+    )
+  ).toMatchSnapshot();
+
+  expect(
+    await getValidationErrors(
+      {
+        price: "$3.86",
+      },
+      "format"
+    )
+  ).toEqual([]);
+});
+
+test(`Doesn't accept nonexistent regions`, async () => {
+  expect(
+    await getValidationErrors(
+      {
+        regionID: "asdfasdf",
+      },
+      "Region does not exist"
+    )
+  ).toMatchInlineSnapshot(`
       [
         {
           "location": "body",
@@ -111,25 +110,25 @@ describe("Test validation", () => {
       ]
     `);
 
-    const realRegionID = (await saveDummyRegion())._id.toString();
-    expect(
-      await getValidationErrors(
-        {
-          regionID: realRegionID,
-        },
-        "Region does not exist"
-      )
-    ).toEqual([]);
-  });
+  const realRegionID = (await saveDummyRegion())._id.toString();
+  expect(
+    await getValidationErrors(
+      {
+        regionID: realRegionID,
+      },
+      "Region does not exist"
+    )
+  ).toEqual([]);
+});
 
-  test(`Doesn't accept duplicate flowers`, async () => {
-    await saveDummyFlower({ name: "duplicate" });
+test(`Doesn't accept duplicate flowers`, async () => {
+  await saveDummyFlower({ name: "duplicate" });
 
-    expect(
-      await getValidationErrors({
-        name: "duplicate",
-      })
-    ).toMatchInlineSnapshot(`
+  expect(
+    await getValidationErrors({
+      name: "duplicate",
+    })
+  ).toMatchInlineSnapshot(`
       [
         {
           "location": "body",
@@ -140,5 +139,4 @@ describe("Test validation", () => {
         },
       ]
     `);
-  });
 });
