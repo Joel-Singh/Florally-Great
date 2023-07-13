a83a70c6339dc44de84d4403e05f2254ab11b3d7
Joel Singh <joelsingh788@gmail.com>
2023-07-13 01:37:56 -0400

    Wrap tests in describe block

--- views/flowers/__tests__/flower_form.test.js
+++ views/flowers/__tests__/flower_form.test.js
@@ -1,48 +1,51 @@
 const renderPugToDOM = require("../../renderPugToDOM.js");
 
-test("Display default values with no passed in preopulated values", () => {
-  expect(getDefaultInputValueAttributesInFlowerForm({})).toMatchInlineSnapshot(`
-    [
-      "",
-      "",
-      "",
-      "",
-    ]
-  `);
-});
-
-test("Display prepopulated values", () => {
-  const locals = {
-    prepopulatedValues: {
-      name: "prepopulated name",
-      description: "preopopulated description",
-      numberInStock: "32",
-      price: "$3.21",
-    },
-  };
-
-  expect(getDefaultInputValueAttributesInFlowerForm(locals))
-    .toMatchInlineSnapshot(`
-    [
-      "prepopulated name",
-      "preopopulated description",
-      "32",
-      "$3.21",
-    ]
-  `);
-});
-
-function getDefaultInputValueAttributesInFlowerForm(locals) {
-  const renderedForm = renderPugToDOM(
-    "./views/flowers/flower_form.pug",
-    locals
-  );
-  let inputs = renderedForm.querySelectorAll("input");
-  inputs = Array.from(inputs);
-
-  const inputValues = inputs.map((element) => {
-    return element.getAttribute("value");
+describe("Prepopulated input values", () => {
+  test("Display default values with no passed in preopulated values", () => {
+    expect(getDefaultInputValueAttributesInFlowerForm({}))
+      .toMatchInlineSnapshot(`
+      [
+        "",
+        "",
+        "",
+        "",
+      ]
+    `);
   });
 
-  return inputValues;
-}
+  test("Display prepopulated values", () => {
+    const locals = {
+      prepopulatedValues: {
+        name: "prepopulated name",
+        description: "preopopulated description",
+        numberInStock: "32",
+        price: "$3.21",
+      },
+    };
+
+    expect(getDefaultInputValueAttributesInFlowerForm(locals))
+      .toMatchInlineSnapshot(`
+      [
+        "prepopulated name",
+        "preopopulated description",
+        "32",
+        "$3.21",
+      ]
+    `);
+  });
+
+  function getDefaultInputValueAttributesInFlowerForm(locals) {
+    const renderedForm = renderPugToDOM(
+      "./views/flowers/flower_form.pug",
+      locals
+    );
+    let inputs = renderedForm.querySelectorAll("input");
+    inputs = Array.from(inputs);
+
+    const inputValues = inputs.map((element) => {
+      return element.getAttribute("value");
+    });
+
+    return inputValues;
+  }
+});
