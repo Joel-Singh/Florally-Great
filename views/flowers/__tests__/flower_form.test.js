9ddbb88bc681cb3c715e3ed6fac9f416736b1177
Joel Singh <joelsingh788@gmail.com>
2023-07-15 00:16:55 -0400

    Add region select test

--- views/flowers/__tests__/flower_form.test.js
+++ views/flowers/__tests__/flower_form.test.js
@@ -1,4 +1,37 @@
 const renderPugToDOM = require("../../renderPugToDOM.js");
+const getValidRegionData = require("./../../../utils/dummyData/getValidData/getValidRegionData.js");
+
+function renderRegionSelect(regionList) {
+  const renderedForm = renderPugToDOM("./views/flowers/flower_form.pug", {
+    regionList,
+  });
+
+  const regionListElement = renderedForm.querySelector(
+    "[placeholder='select region']"
+  );
+
+  return regionListElement;
+}
+
+describe("testing generate region select", () => {
+  test("Region select is empty with no regions", () => {
+    expect(renderRegionSelect([])).toMatchSnapshot();
+  });
+
+  test("Region select has one region", () => {
+    expect(renderRegionSelect([getValidRegionData()])).toMatchSnapshot();
+  });
+
+  test("Region select has multiple regions", () => {
+    expect(
+      renderRegionSelect([
+        getValidRegionData(),
+        getValidRegionData(),
+        getValidRegionData(),
+      ])
+    ).toMatchSnapshot();
+  });
+});
 
 describe("Prepopulated input values", () => {
   test("Display default values with no passed in preopulated values", () => {
