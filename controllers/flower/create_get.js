5f1a8e45676e44ecc4357162c5eb7d5800047f58
Joel Singh <joelsingh788@gmail.com>
2023-07-16 02:31:36 -0400

    Replace repeated code with call to function

--- controllers/flower/create_get.js
+++ controllers/flower/create_get.js
@@ -1,8 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const path = require("path");
-const Region = require(path.join(appRoot, "models", "region.js"));
+const renderFlowerForm = require("./rendersWithDefaultLocals/renderFlowerForm.js");
 
 module.exports = asyncHandler(async (req, res, next) => {
-  const allRegions = await Region.find({}, "name").exec();
-  res.render("flowers/flower_form", { regionList: allRegions });
+  await renderFlowerForm(res, next);
 });
