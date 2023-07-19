0c3d414346c9e784f1041d4a483f378e8b3cce33
Joel Singh <joelsingh788@gmail.com>
2023-07-19 00:00:24 -0400

    Add flower update post

---
+++ controllers/flower/__tests__/update_post.test.js
@@ -0,0 +1,33 @@
+const emulateCallingController = require("./../../testingUtils/emulateCallingController.js");
+const update_post = require("./../update_post.js");
+const saveDummyFlower = require("./../../../utils/dummyData/savingDummyDataToDb/saveDummyFlower.js");
+const saveDummyRegion = require("./../../../utils/dummyData/savingDummyDataToDb/saveDummyRegion.js");
+
+const Flower = require("./../../../models/flower.js");
+
+test("Updates flower in db", async () => {
+  const dummyFlower = (await saveDummyFlower()).toObject({ versionKey: false });
+
+  const flowerPostData = {
+    name: "newName",
+    description: "newDescription",
+    numberInStock: 193,
+    price: "$9.14",
+    regionID: (await saveDummyRegion())._id.toString(),
+  };
+
+  await emulateCallingController(update_post, {
+    body: {
+      ...flowerPostData,
+    },
+  });
+
+  const updatedFlower = await Flower.findById(dummyFlower._id);
+
+  expect(updatedFlower.name).toBe(flowerPostData.name);
+  expect(updatedFlower.description).toBe(flowerPostData.description);
+  expect(updatedFlower.numberInStock).toBe(flowerPostData.numberInStock);
+  expect(updatedFlower.price).toBe(parseFloat(flowerPostData.price.slice(1)));
+
+  expect(updatedFlower.region.toString()).toBe(flowerPostData.regionID);
+});
--- controllers/flower/update_post.js
+++ controllers/flower/update_post.js
@@ -1,5 +1,24 @@
 const asyncHandler = require("express-async-handler");
+const Flower = require("./../../models/flower.js");
+const he = require("he");
 
 module.exports = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Flower update POST");
+  const filter = { id: req.body.id };
+
+  const { name, description, price, numberInStock, regionID } = req.body;
+  const priceToNumber = parseFloat(price.slice(1));
+
+  const convertedName = he.decode(name);
+  const convertedDescription = he.decode(description);
+
+  let update = {
+    name: convertedName,
+    description: convertedDescription,
+    price: priceToNumber,
+    numberInStock,
+    region: regionID,
+  };
+
+  await Flower.updateOne(filter, update);
+  res.send("Update successful");
 });
