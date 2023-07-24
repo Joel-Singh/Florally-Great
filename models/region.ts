d13f5fa58601c9efec801fc6c8856de7f43f1cae
Joel Singh <joelsingh788@gmail.com>
2023-07-23 23:43:31 -0400

    Export types from region.ts

--- models/region.ts
+++ models/region.ts
@@ -1,20 +1,21 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-interface IRegion extends Document {
+export interface IRegionProperties {
   name: string;
   description: string;
-  url: string;
 }
+
+export type IRegionDocument = IRegionProperties & Document & { url: string };
 
 const regionSchema: Schema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
 });
 
-regionSchema.virtual("url").get(function (this: IRegion) {
+regionSchema.virtual("url").get(function (this: IRegionProperties) {
   return `/regions/${this.name}`;
 });
 
-const RegionModel = mongoose.model<IRegion>("Region", regionSchema);
+const RegionModel = mongoose.model<IRegionDocument>("Region", regionSchema);
 
 export default RegionModel;
