eb0051090dd34d4fcc854bbaadc9f96ce682c652
Joel Singh <joelsingh788@gmail.com>
2023-08-01 11:58:14 -0400

    Change IRegionDocument to have _id as a not optional field

--- models/region.ts
+++ models/region.ts
@@ -6,8 +6,14 @@ export interface IRegionProperties {
   _id?: mongoose.Types.ObjectId;
 }
 
-export type IRegionDocument = IRegionProperties &
-  Document<mongoose.Types.ObjectId> & { url: string };
+export type IRegionDocument = Document<
+  mongoose.Types.ObjectId,
+  any,
+  Omit<IRegionProperties, "_id">
+> & {
+  url: string;
+  _id: mongoose.Types.ObjectId;
+};
 
 const regionSchema: Schema = new Schema({
   name: { type: String, required: true },
