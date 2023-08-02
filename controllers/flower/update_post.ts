7842035f0f4d5a5e1e55a46a0ce97de4cccbb6b2
Joel Singh <joelsingh788@gmail.com>
2023-08-02 13:55:29 -0400

    Remove unnecessary non-null assertion operator

--- controllers/flower/update_post.ts
+++ controllers/flower/update_post.ts
@@ -9,7 +9,7 @@ const updateFlowerHandler: RequestHandler = async (
   res,
   next
 ) => {
-  const flowerId: string = req.body.id!;
+  const flowerId: string = req.body.id;
 
   const update = getFlowerModelDataFromReqBody(req);
 
