e883920d06548e9f63fd35635e607f296ea7c0ca
Joel Singh <joelsingh788@gmail.com>
2023-10-21 22:10:29 -0400

    Specify implied any types

--- controllers/region/__tests__/all_flowers_in_region.test.ts
+++ controllers/region/__tests__/all_flowers_in_region.test.ts
@@ -9,9 +9,8 @@ test("Tries rendering correct view", async () => {
 
   await addRegionToDb(regionName);
 
-  const { getRenderInformation } = await simulateInvokingAllFlowersInRegion(
-    regionName
-  );
+  const { getRenderInformation } =
+    await simulateInvokingAllFlowersInRegion(regionName);
   const { view } = getRenderInformation();
 
   expect(view).toMatchInlineSnapshot(`"regions/all_flowers_in_region"`);
@@ -22,9 +21,8 @@ test("Passes in region with empty flower list", async () => {
 
   await addRegionToDb(regionName);
 
-  const { getRenderInformation } = await simulateInvokingAllFlowersInRegion(
-    regionName
-  );
+  const { getRenderInformation } =
+    await simulateInvokingAllFlowersInRegion(regionName);
   const { locals } = getRenderInformation();
 
   expect(locals).toMatchInlineSnapshot(`
@@ -46,9 +44,8 @@ test("Passes in region and flowers", async () => {
   const regionId = await addRegionToDb(regionName);
   await addFlowerToDb(regionId);
 
-  const { getRenderInformation } = await simulateInvokingAllFlowersInRegion(
-    regionName
-  );
+  const { getRenderInformation } =
+    await simulateInvokingAllFlowersInRegion(regionName);
   const { locals } = getRenderInformation();
 
   expect(locals).toMatchInlineSnapshot(`
@@ -77,15 +74,14 @@ test("Passes in region and flowers", async () => {
 test("Calls next with a 404 if region not found", async () => {
   const nonExistingRegion = "IDontExist";
 
-  const { getMockNextInformation } = await simulateInvokingAllFlowersInRegion(
-    nonExistingRegion
-  );
+  const { getMockNextInformation } =
+    await simulateInvokingAllFlowersInRegion(nonExistingRegion);
 
   const { errorOrEmpty } = getMockNextInformation();
   expect(errorOrEmpty.status).toMatchInlineSnapshot(`404`);
 });
 
-async function addRegionToDb(regionName) {
+async function addRegionToDb(regionName: string) {
   const id = "6483d106cdcd7f4f8d6ed46a";
   await new Region({
     name: regionName,
@@ -96,7 +92,7 @@ async function addRegionToDb(regionName) {
   return id;
 }
 
-async function addFlowerToDb(flowersRegionAsId) {
+async function addFlowerToDb(flowersRegionAsId: string) {
   await new Flower({
     name: "Flower in region",
     description: "description",
@@ -107,7 +103,7 @@ async function addFlowerToDb(flowersRegionAsId) {
   }).save();
 }
 
-async function simulateInvokingAllFlowersInRegion(regionName) {
+async function simulateInvokingAllFlowersInRegion(regionName: string) {
   return await emulateCallingController(all_flowers_in_region, {
     params: { name: regionName },
   });
