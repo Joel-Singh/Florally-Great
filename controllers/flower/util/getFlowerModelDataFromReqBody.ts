401cd84ecdbfd11d95b0996ebbf5d014d09b679c
Joel Singh <joelsingh788@gmail.com>
2023-10-26 21:14:53 -0400

    Change getFlowerModelDataFromReqBody to use getDecodedFormValues

--- controllers/flower/util/getFlowerModelDataFromReqBody.ts
+++ controllers/flower/util/getFlowerModelDataFromReqBody.ts
@@ -1,17 +1,19 @@
 import he from "he";
 import { IFlowerProperties } from "../../../models/flower";
 import mongoose from "mongoose";
-import { RequestWithFlowerFormData } from "../../../views/flowers/flowerFormData";
+import { flowerFormKeys } from "../../../views/flowers/flowerFormData";
+import getDecodedFormValues from "../../template/getDecodedFormValues";
+import { Request } from "express";
 
-export default function (req: RequestWithFlowerFormData) {
-  const { name, description, price, numberInStock, regionID } = req.body;
+export default function (req: Request) {
+  const { name, description, price, numberInStock, regionID } =
+    getDecodedFormValues<typeof flowerFormKeys>(req, flowerFormKeys);
+
   const priceToNumber = parseFloat(price.slice(1));
 
-  const convertedName = he.decode(name);
-  const convertedDescription = he.decode(description);
   const modelData: IFlowerProperties = {
-    name: convertedName,
-    description: convertedDescription,
+    name,
+    description,
     price: priceToNumber,
     numberInStock: parseInt(numberInStock),
     region: new mongoose.Types.ObjectId(regionID),
---
+++ controllers/template/getDecodedFormValues.ts
@@ -0,0 +1,14 @@
+import { Request } from "express";
+import he from "he";
+import typeFromStringTuple from "../../views/typeFromStringTuple";
+export default function getDecodedFormValues<keys extends readonly string[]>(
+  req: Request,
+  keys: readonly string[],
+) {
+  let decodedValues: Record<string, string> = {};
+  for (const key of keys) {
+    decodedValues[key] = he.decode(req.body[key]);
+  }
+
+  return decodedValues as typeFromStringTuple<keys>;
+}
