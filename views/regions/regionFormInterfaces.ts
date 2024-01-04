4ccdeb37e6b5261f36be02600ccd55dad69c4b8f
Joel Singh <joelsingh788@gmail.com>
2024-01-04 12:48:24 -0500

    Add RegionFormLocals Interface

--- views/regions/regionFormInterfaces.ts
+++ views/regions/regionFormInterfaces.ts
@@ -1,8 +1,12 @@
 import { Request } from "express";
 import typeFromKeys from "../typeFromKeys";
+import formLocals from "../FormLocals";
 
 export const regionFormKeys = ["name", "description"] as const;
 export type RegionFormData = typeFromKeys<typeof regionFormKeys>;
 export interface RequestWithRegionFormData extends Request {
   body: RegionFormData;
+}
+export interface RegionFormLocals extends formLocals {
+  prepopulatedValues: typeFromKeys<typeof regionFormKeys>
 }
