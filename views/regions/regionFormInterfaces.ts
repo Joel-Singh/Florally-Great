927547fb125cb5b6814973783699a1f607434f4a
Joel Singh <joelsingh788@gmail.com>
2024-01-04 20:18:28 -0500

    Format

--- views/regions/regionFormInterfaces.ts
+++ views/regions/regionFormInterfaces.ts
@@ -3,10 +3,11 @@ import typeFromKeys from "../typeFromKeys";
 import formLocals from "../FormLocals";
 
 export const regionFormKeys = ["name", "description"] as const;
+
 export type RegionFormData = typeFromKeys<typeof regionFormKeys>;
 export interface RequestWithRegionFormData extends Request {
   body: RegionFormData;
 }
 export interface RegionFormLocals extends formLocals {
-  prepopulatedValues: typeFromKeys<typeof regionFormKeys>
+  prepopulatedValues: typeFromKeys<typeof regionFormKeys>;
 }
